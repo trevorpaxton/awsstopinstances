2fb516768339e4b882e267c329b116c48d8f49ab
Sam Bantner <sam.bantner@me.com>
2016-01-14 07:46:00 -0500

    Added some comments

--- lambda.py
+++ lambda.py
@@ -1,7 +1,9 @@
 import boto3
 
+# Does this work?
+# ec2 = boto3.resource('ec2')
+# That may work too... ?
 ec = boto3.client('ec2')
-
 
 def lambda_handler(event, context):
     reservations = ec.describe_instances(
@@ -12,6 +14,7 @@ def lambda_handler(event, context):
         'Reservations', []
     )
 
+    # Is this function only giving you the sum of instances? 
     instances = sum(
         [
             [i for i in r['Instances']]
@@ -28,6 +31,13 @@ def lambda_handler(event, context):
             print "Found EBS volume %s on instance %s" % (
                 vol_id, instance['InstanceId'])
 
+            # This should be the method to stop instances. Where
+            # INSTANCEIDS is an array of the id's of your instances
+            # ec.filter(InstanceIds=INSTANCEIDS).stop()
+            # ec.filter(InstanceIds=INSTANCEIDS).terminate()
             ec2.instances.stop(
                 InstanceIds=ids,
             )
+
+# NOTE: I'd use stronger variable names for better readability.
+# e.g. r['Instances'] -> This doesn't let you know that r is a reservation
